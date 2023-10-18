2660336e294f00b5dc027ddf6013ae6524bd7e64
Bae <bjy556@gmail.com>
2023-10-18 21:53:35 +0900

    calculaotr make

--- controllers/calculator.js
+++ controllers/calculator.js
@@ -14,26 +14,42 @@ module.exports.calculateEmission = async (req, res, next) => {
       if (!recipe) {
         throw new Error(`No recipe found for ${foodname}`);
       }
-      let transport = 0;
+      let productionProcess = 0
+      let transportProcess = 0;
+      let cookingProcess =0;
       let totalEmission = 0;
+      
       //생산과정
       for (let ingredient of recipe.ingredients) {
         const ingredientDoc = await Ingredient.findOne({ ingredient: ingredient.name });
-        console.log(ingredientDoc)
         if (!ingredientDoc) {
           throw new Error(`Ingredient ${ingredient.name} not found`);
         }
 
         // Add to total emission (emission per gram * grams used)
-        totalEmission += (ingredientDoc.emission * ingredient.grams / 1000);
-        //수송과정 식재료 중량(kg) * 수송거리(ton.km) * 탄소성적표지(육상수송(트럭))배출계수
-        transport += ingredient.grams/1000 * 0.18 * 0.249
+        productionProcess += (ingredientDoc.emission * ingredient.grams / 1000);
+        //수송과정
+        // 식재료 중량(kg) * 수송거리(ton.km) * 탄소성적표지(육상수송(트럭))배출계수
+        transportProcess += ingredient.grams/1000 * 0.18 * 0.249
       }
-        console.log("수송",transport)
-      //
-       console.log(recipe.cookingTime)
-
-      console.log(totalEmission)
+      //조리과정
+      for (let cooktime of recipe.cookingTime){
+        if (cooktime.level == 0){
+            cookingProcess += weakFire * cooktime.min
+        }
+        else if(cooktime == 1){
+            cookingProcess += midFire * cooktime.min
+        }
+        else if(cooktime == 2){
+            cookingProcess += strongFire * cooktime.min
+        }
+      }
+      console.log("생산과정",productionProcess);
+      console.log("수송과정",transportProcess);
+      console.log("조리과정",cookingProcess);
+      //전체 탄소배출량
+      totalEmission = productionProcess + transportProcess + cookingProcess
+      console.log("전체탄소배출량",totalEmission)
        //res.json({ totalEmission }); // Send the result back as JSON
       res.send("<script> alert('계산완료.'); location.href='/calculator';</script>");
     } catch (error) {
