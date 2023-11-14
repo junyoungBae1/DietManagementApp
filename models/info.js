8a42a9a02334a6581badef635bfd1c8051a2aa35
Bae <bjy556@gmail.com>
2023-11-14 16:47:32 +0900

    Feat. info 모델 생성

---
+++ models/info.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const DateEmissionSchema = new mongoose.Schema({
+    date: {
+        type: String,
+        required: true,
+    },
+    Dessert: {
+        type: Number,
+        default: 0
+    },
+    Breakfast: {
+        type: Number,
+        default: 0
+    },
+    Lunch: {
+        type: Number,
+        default: 0
+    },
+    Dinner: {
+        type: Number,
+        default: 0
+    },
+    totalEmission:{
+        type: Number,
+        default: 0
+    }
+}, { _id : false });
+
+const InfoSchema = new mongoose.Schema({
+    email: {
+        type : String,
+        required : true,
+        unique: true,
+    },
+    dates: [DateEmissionSchema]
+
+});
+
+module.exports = mongoose.model("Info", InfoSchema);
